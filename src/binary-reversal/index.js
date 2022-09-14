7f311a5921fb06825b747a2872f30ce51c9ee36a
Akem-Ben <107713743+Akem-Ben@users.noreply.github.com>
2022-09-14 16:13:09 +0100

    Akemini
    Akemini

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,12 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+    const convert = parseInt(value).toString(2).padStart(8,0).split("").reverse().join("") /*This is responsible for converting the inputted value (which is a string) to an integer. 
+    The parseInt method will parse a string, then returns an integer. The .tostring(2) method converts the parsed integer into binary. 
+    The binary number is padded and then converted to an array (.split("")), reversed and converted back to a string (.join("")).*/
+    const result = parseInt(convert,2); //This line then parses the new string and it is returned as a binary integer.
+    return result.toString()
+}
 
 module.exports = binaryReversal;
--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,23 @@
-function listSorting(needle, haystack) {}
-
+function listSorting(needle, haystack) {
+    //We declare our required variables
+    let lastArray = -1;
+    let lastIndex;
+//We loop through the haystack array
+    for(let i = 0; i < haystack.length; i++){
+      const a = haystack[i]
+        if(a.includes(needle)){
+            lastArray = i;
+            lastIndex = a.lastIndexOf(needle)
+        }
+    }
+    if(lastArray != -1){
+        return [lastArray, lastIndex]
+    }
+    else {
+        return -1
+    }
+if(typeof haystack[0] != array){
+  return haystack.lastIndexOf(needle)
+}
+}
 module.exports = listSorting;
